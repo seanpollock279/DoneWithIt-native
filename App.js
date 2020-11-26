d2fa3425a5e8000a1346e253a6e9037c91ca290d
Sean Pollock <seanpollock@Seans-MacBook-Air-2.local>
2020-11-26 12:12:05 -0500

    app coming together

--- App.js
+++ App.js
@@ -13,12 +13,13 @@ import Screen from './app/components/Screen';
 import Icon from './app/components/Icon';
 import ListItem from './app/components/ListItem';
 import AccountScreen from './app/screens/AccountScreen';
+import ListingsScreen from './app/screens/ListingsScreen';
 
 import AppText from './app/components/AppText';
 
 export default function App() {
   return (
-    <AccountScreen />
+    <ListingsScreen />
   );
 }
 
