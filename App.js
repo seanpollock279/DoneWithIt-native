a968e3a514b98a0466ded8c418e2f9580f63a4ec
Sean Pollock <seanpollock@Seans-Mac-mini.local>
2021-02-08 16:45:37 -0500

    listingeditscreen added, still some extra stuff to do

--- App.js
+++ App.js
@@ -5,25 +5,10 @@ import WelcomeScreen from './app/screens/WelcomeScreen';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-import AppButton from './app/components/AppButton';
-import Card from './app/components/Card';
-import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
-import MessagesScreen from './app/screens/MessagesScreen';
-import Screen from './app/components/Screen';
-import Icon from './app/components/Icon';
-import ListItem from './app/components/ListItem';
-import AccountScreen from './app/screens/AccountScreen';
-import ListingsScreen from './app/screens/ListingsScreen';
-import AppTextInput from './app/components/AppTextInput';
-import AppPicker from './app/components/AppPicker';
-import LoginScreen from './app/screens/LoginScreen';
-import RegisterScreen from './app/screens/RegisterScreen';
-
-import AppText from './app/components/AppText';
+import ListingEditScreen from './app/screens/ListingEditScreen';
 
 export default function App() {
-  return (
-    <RegisterScreen />
-  );
+  return <ListingEditScreen />;
+  
 }
 
